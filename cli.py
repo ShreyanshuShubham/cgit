355249683c2d17edee0fa9b3fedad411fcf8d108
ShreyanshuShubham <shreyanshu.dps@gmail.com>
2024-11-03 21:25:57 +0000

    added code to find-root

--- cli.py
+++ cli.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 import hashlib
 import os
@@ -22,28 +24,33 @@ def main():
      hash_object_parser.add_argument('-t',required=False,action="store",choices=["blob","tree","commit"],help="Type of hash",default="blob")
      hash_object_parser.add_argument('-stdin',required=False,action="store_true",help="Read content from standard in")
 
+     find_root_parser = commands.add_parser("find-root")
+
      cat_file_parser = commands.add_parser("cat-file")
      cat_file_parser.add_argument('hash')
+
+
      
      ARGS = parser.parse_args()
      
      match ARGS.command:
-        case "add"          : cmd_add(ARGS)
-        case "cat-file"     : cmd_cat_file(ARGS)
-        case "check-ignore" : cmd_check_ignore(ARGS)
-        case "checkout"     : cmd_checkout(ARGS)
-        case "commit"       : cmd_commit(ARGS)
-        case "hash-object"  : cmd_hash_object(ARGS)
-        case "init"         : cmd_init(ARGS)
-        case "log"          : cmd_log(ARGS)
-        case "ls-files"     : cmd_ls_files(ARGS)
-        case "ls-tree"      : cmd_ls_tree(ARGS)
-        case "rev-parse"    : cmd_rev_parse(ARGS)
-        case "rm"           : cmd_rm(ARGS)
-        case "show-ref"     : cmd_show_ref(ARGS)
-        case "status"       : cmd_status(ARGS)
-        case "tag"          : cmd_tag(ARGS)
-        case _              : printerr("Bad cgit command.")
+          case "add"          : cmd_add(ARGS)
+          case "cat-file"     : cmd_cat_file(ARGS)
+          case "check-ignore" : cmd_check_ignore(ARGS)
+          case "checkout"     : cmd_checkout(ARGS)
+          case "commit"       : cmd_commit(ARGS)
+          case "hash-object"  : cmd_hash_object(ARGS)
+          case "init"         : cmd_init(ARGS)
+          case "log"          : cmd_log(ARGS)
+          case "ls-files"     : cmd_ls_files(ARGS)
+          case "ls-tree"      : cmd_ls_tree(ARGS)
+          case "rev-parse"    : cmd_rev_parse(ARGS)
+          case "rm"           : cmd_rm(ARGS)
+          case "show-ref"     : cmd_show_ref(ARGS)
+          case "status"       : cmd_status(ARGS)
+          case "tag"          : cmd_tag(ARGS)
+          case "find-root"    : cmd_find_root(ARGS)
+          case _              : printerr("Bad cgit command.")
 
 def cmd_add(): pass
 def cmd_check_ignore(): pass
@@ -57,6 +64,19 @@ def cmd_rm(): pass
 def cmd_show_ref(): pass
 def cmd_status(): pass
 def cmd_tag(): pass
+
+def util_find_root() -> str|None:
+     current_directory=os.path.realpath(".")
+     parent_directory = os.path.realpath(os.path.join(current_directory,".."))
+     while current_directory != parent_directory:
+          if os.path.isdir(os.path.join(current_directory,".cgit")):
+               return current_directory
+          current_directory = parent_directory
+          parent_directory = os.path.realpath(os.path.join(current_directory,".."))
+     return None
+
+def cmd_find_root(ARGS:argparse.Namespace) -> None:
+     print(util_find_root())
 
 def cmd_init(ARGS:argparse.Namespace):
      if not os.path.exists(ARGS.path):
